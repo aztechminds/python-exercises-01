e8dcb302f66096c7c08828312d700d86c31f9411
techminds <aztechminds@gmail.com>
2024-10-01 15:04:36 +0200

    Übungsaufgabe 1 erledigt

---
+++ python-grundlagen1.py
@@ -0,0 +1,13 @@
+# Übungsaufgabe 1: Python Grundlagen 
+
+# Definiere eine Variable First_name mit meinem Vornamen
+first_name = "Abdullah"
+last_name = "Zahed"
+birth_year = "1992"
+has_license = "True"
+
+#Ausgabe der Variablen
+print ("Vorname:" , first_name)
+print("Nachname:" , last_name)
+print("Geburtsjahr:" , birth_year)
+print("Hat Führerschein", has_license)
